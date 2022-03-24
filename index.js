4e649ab727ae451d66ad6b4428bad7d2dcbcc8ad
shubham397 <shubhamkumar397@gmail.com>
2022-03-24 13:59:30 +0530

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+const PORT = 3000;
+
+app.get("/", function (req, res) {
+  res.send("Home");
+});
+
+app.get("/client/:clientId", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+http.listen(PORT, function () {
+  console.log("listening on *:" + PORT);
+});
+
+io.on("connection", function (socket) {
+  console.log(socket.Socket);
+  console.log("user connected");
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+
+  socket.on("message-send-event", function (message) {
+    console.log(message);
+    io.emit("update-message", message);
+  });
+});
